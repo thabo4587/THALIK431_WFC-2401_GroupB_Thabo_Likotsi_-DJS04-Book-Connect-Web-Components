02681383054efd846c665ca86ea0f55264537be0
Thabo Likotsi <142932054+thabo4587@users.noreply.github.com>
2024-05-18 18:31:53 +0200

    Update scripts.js
    Code with updated CSS more changes to follow

--- DJS04 Web Components/scripts.js
+++ DJS04 Web Components/scripts.js
@@ -7,32 +7,54 @@ class BookPreview extends HTMLElement {
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = `
             <style>
-                .preview {
-                    display: flex;
-                    align-items: center;
-                    padding: 10px;
-                    border: 1px solid #ccc;
-                    margin-bottom: 10px;
-                    cursor: pointer;
-                }
-                .preview__image {
-                    width: 50px;
-                    height: 75px;
-                    object-fit: cover;
-                    margin-right: 10px;
-                }
-                .preview__info {
-                    display: flex;
-                    flex-direction: column;
-                }
-                .preview__title {
-                    font-size: 1.1em;
-                    margin: 0;
-                }
-                .preview__author {
-                    font-size: 0.9em;
-                    color: #555;
-                }
+            .preview {
+                border-width: 0;
+                width: 100%;
+                font-family: Roboto, sans-serif;
+                padding: 0.5rem 1rem;
+                display: flex;
+                align-items: center;
+                cursor: pointer;
+                text-align: left;
+                border-radius: 8px;
+                border: 1px solid rgba(var(--color-dark), 0.15);
+                background: rgba(var(--color-light), 1);
+              }
+              .preview_hidden {
+                display: none;
+              }
+              
+              .preview:hover {
+                background: rgba(var(--color-blue), 0.05);
+              }
+              
+              .preview__image {
+                width: 48px;
+                height: 70px;
+                object-fit: cover;
+                background: grey;
+                border-radius: 2px;
+                box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+                  0px 1px 1px 0px rgba(0, 0, 0, 0.1), 0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+              }
+              
+              .preview__info {
+                padding: 1rem;
+              }
+              
+              .preview__title {
+                margin: 0 0 0.5rem;
+                font-weight: bold;
+                display: -webkit-box;
+                -webkit-line-clamp: 2;
+                -webkit-box-orient: vertical;  
+                overflow: hidden;
+                color: rgba(var(--color-dark), 0.8)
+              }
+              
+              .preview__author {
+                color: rgba(var(--color-dark), 0.4);
+              }
             </style>
             <button class="preview" data-preview>
                 <img class="preview__image" />
